97961ad825e77a5149969e2cc42f950e7eb1b1bc
bestehle <bestehle@htwg-konstanz.de>
2014-06-05 09:21:40 +0200

    added hashCode and equals

--- src/main/java/de/seerhein_lab/jic/AnalysisResult.java
+++ src/main/java/de/seerhein_lab/jic/AnalysisResult.java
@@ -45,4 +45,40 @@ public class AnalysisResult {
 		return analyzedMethod;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((analyzedMethod == null) ? 0 : analyzedMethod.hashCode());
+		result = prime * result + ((bugs == null) ? 0 : bugs.hashCode());
+		result = prime * result + ((results == null) ? 0 : results.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AnalysisResult other = (AnalysisResult) obj;
+		if (analyzedMethod == null) {
+			if (other.analyzedMethod != null)
+				return false;
+		} else if (!analyzedMethod.equals(other.analyzedMethod))
+			return false;
+		if (bugs == null) {
+			if (other.bugs != null)
+				return false;
+		} else if (!bugs.equals(other.bugs))
+			return false;
+		if (results == null) {
+			if (other.results != null)
+				return false;
+		} else if (!results.equals(other.results))
+			return false;
+		return true;
+	}
 }
