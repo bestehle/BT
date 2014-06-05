010cb44b71d8a46b10e944cb647bb7c6fca504dc
bestehle <bestehle@htwg-konstanz.de>
2014-06-05 09:07:50 +0200

    logging: added lists for Results of Analyzation

--- src/test/java/playground/ConfinementTestDriver.java
+++ src/test/java/playground/ConfinementTestDriver.java
@@ -2,8 +2,8 @@ package playground;
 
 import java.io.IOException;
 import java.util.Collection;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -12,7 +12,6 @@ import org.apache.bcel.classfile.JavaClass;
 import de.seerhein_lab.jic.AnalysisResult;
 import de.seerhein_lab.jic.ClassRepository;
 import de.seerhein_lab.jic.EmercencyBrakeException;
-import de.seerhein_lab.jic.EvaluationResult;
 import de.seerhein_lab.jic.Utils;
 import edu.umd.cs.findbugs.BugInstance;
 import edu.umd.cs.findbugs.MethodAnnotation;
@@ -21,11 +20,16 @@ import edu.umd.cs.findbugs.StringAnnotation;
 public class ConfinementTestDriver {
 	private static final String LOGFILEPATH = "log.txt";
 	private static Logger logger;
-	private static Set<EvaluationResult> results = new HashSet<EvaluationResult>();
+	// private static Set<EvaluationResult> results = new
+	// HashSet<EvaluationResult>();
+	private static Set<String> stackConfinedClasses = new TreeSet<String>();
+	private static Set<String> notStackConfinedClasses = new TreeSet<String>();
+	private static Set<String> notAnalyzedClasses = new TreeSet<String>();
+	private static Set<String> notInstantiatedClasses = new TreeSet<String>();
 
 	public static void main(String[] args) throws ClassNotFoundException, SecurityException,
 			IOException {
-		logger = Utils.setUpLogger("ConfinementTestDriver", LOGFILEPATH, Level.ALL);
+		logger = Utils.setUpLogger("ConfinementTestDriver", LOGFILEPATH, Level.SEVERE);
 
 		// String class_name =
 		// "de.seerhein_lab.jic.analyzer.StackConfinementAcceptanceTest";
@@ -35,7 +39,8 @@ public class ConfinementTestDriver {
 		// Set<JavaClass> classes =
 		// ClassRepository.getClassWithInnerClasses(class_name);
 
-		String classToAnalyze = "de.seerhein_lab.jic.analyzer.StackConfinementAcceptanceTest$TestClass";
+		// String classToAnalyze =
+		// "de.seerhein_lab.jic.analyzer.StackConfinementAcceptanceTest$TestClass";
 		Collection<JavaClass> classes = ClassRepository.getClasses("de.seerhein_lab.jic");
 
 		// Set<AnalysisResult> analysisResults = analyze(classToAnalyze,
@@ -48,22 +53,44 @@ public class ConfinementTestDriver {
 	}
 
 	private static void logResults(String classToAnalyze, Set<AnalysisResult> analysisResults) {
-		logger.severe("Stack Confinement of " + classToAnalyze);
+		boolean stackConfined = true;
+		logger.severe(String
+				.format("\n###############################################################################\n"
+						+ "#    %-65s        #"
+						+ "\n#-----------------------------------------------------------------------------#\n#",
+						classToAnalyze));
+		// logger.severe("Stack Confinement of " + classToAnalyze);
 		for (AnalysisResult analysisResult : analysisResults) {
-			if (analysisResult.getBugs().isEmpty())
-				logger.severe("\t Stack Confined\t\t : " + analysisResult.getAnalyzedMethod());
-			else
+			if (analysisResult.getBugs().isEmpty()) {
+				logger.severe("#\t Stack Confined\t\t : " + analysisResult.getAnalyzedMethod());
+			} else {
 				for (BugInstance bugInstance : analysisResult.getBugs()) {
 					MethodAnnotation method = bugInstance.getAnnotationWithRole(
 							MethodAnnotation.class, "METHOD_DEFAULT");
-					logger.severe("\t NOT Stack Confined\t : " + method);
-					logger.severe("\t\t (Line "
+					logger.severe("#\t NOT Stack Confined\t : " + method);
+					logger.severe("#\t\t (Line "
 							+ method.getSourceLines().getStartLine()
 							+ ") -> "
 							+ bugInstance.getAnnotationWithRole(StringAnnotation.class,
 									"STRING_DEFAULT"));
 				}
+				stackConfined = false;
+			}
 		}
+		logger.severe(String
+				.format("#\n#-----------------------------------------------------------------------------#\n"
+						+ "#    %-70s   #"
+						+ "\n###############################################################################\n",
+						classToAnalyze
+								+ (stackConfined ? ":   Stack Confined" : ":   NOT Stack Confined")));
+
+		if (analysisResults.isEmpty())
+			notInstantiatedClasses.add(classToAnalyze);
+		else if (stackConfined)
+			stackConfinedClasses.add(classToAnalyze);
+		else
+			notStackConfinedClasses.add(classToAnalyze);
+
 	}
 
 	public static Set<AnalysisResult> analyze(String classToCheck,
@@ -85,8 +112,25 @@ public class ConfinementTestDriver {
 				logResults(javaClass.getClassName(), ClassRepository.analyzeMethods(repository
 						.getClass(javaClass.getClassName())));
 			} catch (EmercencyBrakeException e) {
-				logger.severe(e.toString());
+				notAnalyzedClasses.add(javaClass.getClassName());
 			}
 		}
+
+		logSummary("Stack Confined Classes", stackConfinedClasses);
+		logSummary("NOT Stack Confined Classes", notStackConfinedClasses);
+		logSummary("NOT Instantiated Classes", notInstantiatedClasses);
+		logSummary("Not analyzed Classes", notAnalyzedClasses);
+	}
+
+	private static void logSummary(String list, Set<String> classes) {
+		logger.severe(String
+				.format("\n###############################################################################\n"
+						+ "#    %2d   %-60s        #"
+						+ "\n#-----------------------------------------------------------------------------#",
+						classes.size(), list));
+		for (String clazz : classes) {
+			logger.severe(String.format("#    %-65s        #", clazz));
+		}
+		logger.severe("###############################################################################\n");
 	}
 }
